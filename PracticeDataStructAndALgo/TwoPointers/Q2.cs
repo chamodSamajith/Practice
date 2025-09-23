6ab3958b715d5a82b31b1092c510686d351a357f
chamodSamajith <chamodsr@gmail.com>
2025-09-23 23:09:31 +1000

    expected out put

--- PracticeDataStructAndALgo/TwoPointers/Q2.cs
+++ PracticeDataStructAndALgo/TwoPointers/Q2.cs
@@ -10,6 +10,12 @@ namespace PracticeDataStructAndALgo.TwoPointers
             var numbers = new int[] { -5, -2, 0, 1, 2, 3, 5, 7, 8, 10 };
             int target = 5;
             //Using the two pointers technique, find all unique pairs of numbers that add up to the target.
+            //             Expected Output
+            // For target = 5, the pairs should be:
+            // (-2, 7)
+            // (0, 5)
+            // (2, 3)
+            
         }
 
     }
