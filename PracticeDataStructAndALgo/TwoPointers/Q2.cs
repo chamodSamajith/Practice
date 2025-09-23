4c5358043f36cbdb0a5f2a48f258199767b82506
chamodSamajith <chamodsr@gmail.com>
2025-09-23 23:08:11 +1000

    two pointer class updates

--- PracticeDataStructAndALgo/TwoPointers/Q2.cs
+++ PracticeDataStructAndALgo/TwoPointers/Q2.cs
@@ -4,16 +4,6 @@ namespace PracticeDataStructAndALgo.TwoPointers
 {
     public class Q2
     {
-        // Constructor
-        public Q2()
-        {
-            // Initialization code here
-        }
 
-        // Example method
-        public void Run()
-        {
-            Console.WriteLine("Q2 class is running.");
-        }
     }
 }
