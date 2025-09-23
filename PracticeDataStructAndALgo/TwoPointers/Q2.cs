064b942d563a35ba3909725271b43cbf021feffe
chamodSamajith <chamodsr@gmail.com>
2025-09-23 23:08:59 +1000

    class for q2

--- PracticeDataStructAndALgo/TwoPointers/Q2.cs
+++ PracticeDataStructAndALgo/TwoPointers/Q2.cs
@@ -5,5 +5,12 @@ namespace PracticeDataStructAndALgo.TwoPointers
     public class Q2
     {
 
+        public static void Run()
+        {
+            var numbers = new int[] { -5, -2, 0, 1, 2, 3, 5, 7, 8, 10 };
+            int target = 5;
+            //Using the two pointers technique, find all unique pairs of numbers that add up to the target.
+        }
+
     }
 }
